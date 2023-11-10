1299a1c7e0af29e7ee9b7e03cb8ac7e08bba77d5
genie-ru <genie.zgmf987@gmail.com>
2023-11-11 07:05:41 +0900

    test

--- cpuls_beginner/chap04/main_6.cpp
+++ cpuls_beginner/chap04/main_6.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int n[5];
 
-    cout << "&n[2]      = " << (size_t)&n[2]        << endl;
-         << "&n[2]      = " << (size_t)(&n[2] + 1)  << endl;
+    cout << "&n[2]      = " << (size_t)&n[2]        << endl
+         << "&n[2]      = " << (size_t)(&n[2] + 1)  << endl
          << "&n[3]      = " << (size_t)&n[3]        << endl;
 
     return 0;
